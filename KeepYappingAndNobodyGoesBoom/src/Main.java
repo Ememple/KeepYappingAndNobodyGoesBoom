0928538d2035b38e45b505700a8b618e9f2295ee
Boootyshaker9000 <relich@post.cz>
2024-05-05 00:20:32 +0200

    Vytvoření framu a panelu v Mainu.

--- KeepYappingAndNobodyGoesBoom/src/Main.java
+++ KeepYappingAndNobodyGoesBoom/src/Main.java
@@ -1,5 +1,27 @@
+import javax.swing.*;
+import java.awt.*;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("🤓");
+
+        JFrame frame = new JFrame();
+        frame.setTitle("Keep yapping and nobody goes BOOM!");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setLayout(null);
+        frame.setResizable(false);
+        frame.setSize(1920, 1080);
+        frame.setVisible(true);
+
+        ImageIcon icon = new ImageIcon("icon.jpeg");
+        frame.setIconImage(icon.getImage());
+        frame.getContentPane().setBackground(Color.BLACK);
+
+        JPanel blue_panel = new JPanel();
+        blue_panel.setBackground(Color.BLUE);
+        blue_panel.setBounds(0,0, 1920/6, 1080/6);
+
+
+        frame.add(blue_panel);
     }
 }
