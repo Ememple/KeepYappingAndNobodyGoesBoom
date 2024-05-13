3ecd36e8f29490f05d930b5123822c308c6598ec
Boootyshaker9000 <relich@post.cz>
2024-05-13 17:59:39 +0200

    Přidání tlačítek UpButton.java a DownButton.java Přidání metod do třídy Password.java

--- KeepYappingAndNobodyGoesBoom/src/Modules/Password.java
+++ KeepYappingAndNobodyGoesBoom/src/Modules/Password.java
@@ -3,6 +3,7 @@ package Modules;
 import Modules.PasswordButtons.DownButton;
 import Modules.PasswordButtons.UpButton;
 
+import javax.swing.*;
 import java.awt.*;
 import java.io.BufferedReader;
 import java.io.FileReader;
@@ -11,19 +12,26 @@ import java.io.IOException;
 public class Password extends Module{
     private UpButton upButton;
     private DownButton downButton;
-    private int rows = 5;
-    private int columns = 4;
+    private int rows = 4;
+    private int columns = 5;
     private char[][] chars;
     private String[] passwords;
     private String password;
+    private JLabel label;
 
     public Password() throws IOException {
         chars = new char[rows][columns];
         passwords = new String[35];
         addIntoPasswords();
         password = passwords[getRandom().nextInt(35)];
+        upButton = new UpButton();
+        downButton = new DownButton();
         addIntoChars();
-        setLayout(new GridLayout(2,3));
+        setLayout(new GridLayout(3,5));
+        addUpButtons();
+        addCharsComponent();
+        addDownButtons();
+        setVisible(true);
     }
 
     public void addIntoPasswords() throws IOException {
@@ -38,10 +46,28 @@ public class Password extends Module{
             chars[i][0] = password.charAt(i);
         }
 
-        for (int i = 0; i < columns; i++) {
-            for (int ii = 1; ii < rows; ii++) {
+        for (int i = 0; i < rows; i++) {
+            for (int ii = 1; ii < columns; ii++) {
                 chars[i][ii] = (char) getRandom().nextInt(97,123);
             }
+        }
+    }
+
+    public void addUpButtons(){
+        for (int i = 0; i < 5; i++) {
+            add(upButton);
+        }
+    }
+
+    public void addCharsComponent(){
+        for (int i = 0; i < 5; i++){
+            add(new JLabel(String.valueOf(chars[1][getRandom().nextInt(4)])));
+        }
+    }
+
+    public void addDownButtons(){
+        for (int i = 0; i < 5; i++) {
+            add(downButton);
         }
     }
 }
