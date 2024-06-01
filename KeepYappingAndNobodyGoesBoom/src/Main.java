d9d521c8dab8c27c9dd8a40f5b9ce53eebe7ae3f
Ememple <honzacihar@email.cz>
2024-06-01 13:55:38 +0200

    oprava toho co Zdeněk rozbil

--- KeepYappingAndNobodyGoesBoom/src/Main.java
+++ KeepYappingAndNobodyGoesBoom/src/Main.java
@@ -1,10 +1,7 @@
 import Frames.Menu;
-import Frames.WinScreen;
-
-import java.io.IOException;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args){
         Menu menu = new Menu();
         menu.createMenu();
     }
