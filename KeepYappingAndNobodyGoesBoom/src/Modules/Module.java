546ec4e06ac48bdd0bfbf26a6b1cd8ef00c5ffb3
Boootyshaker9000 <relich@post.cz>
2024-06-01 14:50:37 +0200

    Smazání zbytešných výpisů.

--- KeepYappingAndNobodyGoesBoom/src/Modules/Module.java
+++ KeepYappingAndNobodyGoesBoom/src/Modules/Module.java
@@ -9,12 +9,10 @@ import java.util.Random;
 public abstract class Module extends JPanel {
     private Bomb bomb;
     private Random random;
-    //private boolean solved;
 
     public Module(Bomb bomb) {
         super();
         this.bomb = bomb;
-        //solved = false;
         this.random = new Random();
         setBorder(BorderFactory.createLineBorder(Color.BLACK,10,false));
     }
@@ -26,11 +24,8 @@ public abstract class Module extends JPanel {
     public void disableModule(){
         for (int i = 0; i < getComponents().length; i++) {
             getComponent(i).setEnabled(false);
-            //getComponent(i).setBackground(Color.GREEN);
         }
         setEnabled(false);
-        //solved = true;
-        //setBackground(Color.GREEN);
     }
 
     public Bomb getBomb() {
@@ -40,16 +35,4 @@ public abstract class Module extends JPanel {
     public void setBomb(Bomb bomb) {
         this.bomb = bomb;
     }
-
-    public void setRandom(Random random) {
-        this.random = random;
-    }
-
-    /*public boolean isSolved() {
-        return solved;
-    }
-
-    public void setSolved(boolean solved) {
-        this.solved = solved;
-    }*/
 }
